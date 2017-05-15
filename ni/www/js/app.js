b9794ead4ef0e3a8facfb80118c56bc51ee517bc
CheSyu\Syu <choijh00623@gmail.com>
2017-05-15 15:51:06 +0900

    사이드 슬라이드메뉴 성공.  로그인페이지 뷰 만들기 성공,  슬라이드메뉴&티쳐메인6박스 CSS 수정본

--- ni/www/js/app.js
+++ ni/www/js/app.js
@@ -1,11 +1,31 @@
 $(document).ready(function(){
 
+  // $('.notLoginTop').show();
+  // $('.loginMainBox').show();
+  // $('.LoginBtn').show();
+
   $('.sideMenu').show();
   $('.sideMenuListDiv').hide();
   $('.top').show();
   $('.topMenu').show();
   $('.mainContent').show();
 
+  // 교사아이디로 로그인버튼 클릭
+  $('.LoginBtn').click(function(){
+
+    $('.notLoginTop').hide();
+    $('.loginMainBox').hide();
+    $('.LoginBtn').hide();
+
+    $('.sideMenu').show();
+    $('.sideMenuListDiv').hide();
+    $('.top').show();
+    $('.topMenu').show();
+    $('.mainContent').show();
+
+});
+
+// 사이드메뉴 슬라이드
   $(".sideMenuBtn").click(function(){
       $(".sideMenuListDiv").toggle("slide",{direction:"left"},500,null);
   });
@@ -13,5 +33,20 @@ $(document).ready(function(){
   $(".mainContent").click(function(){
       $(".sideMenuListDiv").hide("slide",{direction:"left"},500,null);
   });
+
+
+  // 로그아웃 클릭
+  $(".logoutBtn").click(function(){
+    $('.sideMenu').hide();
+    $('.sideMenuListDiv').hide();
+    $('.top').hide();
+    $('.topMenu').hide();
+    $('.mainContent').hide();
+
+    $('.notLoginTop').show();
+    $('.loginMainBox').show();
+    $('.LoginBtn').show();
+  });
+
 
 });
